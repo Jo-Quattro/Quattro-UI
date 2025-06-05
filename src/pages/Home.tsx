2a06a06c1d3d59198a8ddc691f368f32654b9637
Jo-Quattro <Jordan.aulagnier@gmail.com>
2025-06-05 14:28:57 +0200

    before merge

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import CarouselComponent from "../components/CarouselComponent";
 
 function Home() {
 	return (
-		<section className="bg-gray-950 text-white flex flex-col items-center justify-center h-screen">
+		<section className="bg-gray-950 text-white flex flex-col items-center justify-center h-1000">
 			<CarouselComponent />
 		</section>
 	);
