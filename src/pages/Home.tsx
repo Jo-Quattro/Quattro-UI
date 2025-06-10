6deb478329862a212d74882df0cfcad2f9b31ed8
Jo-Quattro <Jordan.aulagnier@gmail.com>
2025-06-10 13:04:06 +0200

    changed the paragraph so they just appear by block and not by letters

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ const homeSubtitle =
 const paragraph1 =
   "Quattro-UI is my personal space to learn by doing, and to share everything I build along the way. Each component has been carefully recreated from tutorials, videos, or real-world examples, then adapted with a focus on reusability and simplicity. Whether you’re just starting out or looking for a clean starting point for your own designs, these components are meant to be practical and easy to integrate.";
 const paragraph2 =
-  "I’m still learning — and I probably make mistakes. But this project is open: open to improvements, open to inspiration, and open to anyone whowants to contribute or borrow. If you see something worth enhancing,  feel free to suggest changes or share your own versions. Wherever possible, I credit the original creators who inspired me. Quattro-UI isn’t about reinventing the wheel — it’s about making good ideas more usable, together.";
+  "I’m still learning — and I probably make mistakes. But this project is open : open to improvements, open to inspiration, and open to anyone whowants to contribute or borrow. If you see something worth enhancing,  feel free to suggest changes or share your own versions. Wherever possible, I credit the original creators who inspired me. Quattro-UI isn’t about reinventing the wheel — it’s about making good ideas more usable, together.";
 
 const blurEffect = (index: number) => ({
   initial: { filter: "blur(10px)", opacity: 0, y: 10 },
@@ -50,7 +50,7 @@ function Home() {
   let baseIndex = 0;
 
   return (
-    <section className="w-full h-[100%] text-center px-15 flex flex-col gap-8 my-14">
+    <section className="w-full h-[100%] text-center px-15 flex flex-col items-center gap-8 my-14">
       <h2 className="font-semibold italic text-4xl">
         {(() => {
           const output = renderAnimatedText(homeTitle, baseIndex);
@@ -58,7 +58,6 @@ function Home() {
           return output;
         })()}
       </h2>
-
       <h3 className="text-xl font-medium">
         {(() => {
           const output = renderAnimatedText(homeSubtitle, baseIndex);
@@ -67,28 +66,29 @@ function Home() {
         })()}
       </h3>
       <motion.section
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 3.5, delay: 0.03 * baseIndex + 0.3 }}
+        initial={{ opacity: 0, filter: "blur(5px)" }}
+        animate={{ opacity: 1, filter: "blur(0)" }}
+        transition={{ duration: 3, delay: 0.03 * baseIndex + 0.3 }}
         className="mt-3 overflow-hidden"
       >
         <CarouselComponent />
       </motion.section>
-      <p className=" leading-relaxed mt-4">
-        {(() => {
-          const output = renderAnimatedText(paragraph1, baseIndex + 45);
-          baseIndex = advanceIndex(baseIndex, paragraph1.length);
-          return output;
-        })()}
-      </p>
-
-      <p className=" leading-relaxed">
-        {(() => {
-          const output = renderAnimatedText(paragraph2, baseIndex + 5);
-          baseIndex = advanceIndex(baseIndex, paragraph2.length);
-          return output;
-        })()}
-      </p>
+      <motion.span
+        initial={{ filter: "blur(10px)", opacity: 0, x: -100 }}
+        animate={{ filter: "blur(0)", opacity: 1, x: 0 }}
+        transition={{ duration: 1, delay: 0.03 * baseIndex + 1 }}
+        className="text-left px-5 leading-relaxed mt-4"
+      >
+        {paragraph1}
+      </motion.span>
+      <motion.span
+        initial={{ filter: "blur(10px)", opacity: 0, x: -100 }}
+        animate={{ filter: "blur(0)", opacity: 1, x: 0 }}
+        transition={{ duration: 1, delay: 0.03 * baseIndex + 1.9 }}
+        className="text-left px-5 leading-relaxed mt-4"
+      >
+        {paragraph2}
+      </motion.span>
     </section>
   );
 }
